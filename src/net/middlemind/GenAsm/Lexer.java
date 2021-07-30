e666404a5df49b12b19bf269006f38db4cf8c6be
Victor G. Brusca <brusca@middlemind.net>
2021-07-30 15:18:14 -0400

    Updated project with the groundwork for simple lexer.

--- src/net/middlemind/GenAsm/Lexer.java
+++ src/net/middlemind/GenAsm/Lexer.java
@@ -4,6 +4,6 @@ package net.middlemind.GenAsm;
  *
  * @author Victor G. Brusca, Middlemind Games 07-19-2021 7:05 PM EST
  */
-public class Lexer {
-    
+public interface Lexer {
+    public String[] LineLexerizer(String line);
 }
---
+++ src/net/middlemind/GenAsm/LexerSimple.java
@@ -0,0 +1,14 @@
+package net.middlemind.GenAsm;
+
+/**
+ *
+ * @author Victor G. Brusca, Middlemind Games 07/30/2021 3:17 PM EST
+ */
+public class LexerSimple implements Lexer {
+
+    @Override
+    public String[] LineLexerize(String line) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
