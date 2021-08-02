1b2eec209439771546277a4a2456676568d507bc
Victor G. Brusca <brusca@middlemind.net>
2021-08-02 13:29:01 -0400

    Updated project with the latest changes to support prepping json files for java object creation.

---
+++ src/net/middlemind/GenAsm/JsonObjIsRegister.java
@@ -0,0 +1,14 @@
+package net.middlemind.GenAsm;
+
+/**
+ *
+ * @author Victor G. Brusca, Middlemind Games 08/02/2021 12:58 PM EST
+ */
+public class JsonObjIsRegister {
+    public String register_name;
+    public String obj_name;
+    public String bit_rep_str;
+    public int bit_rep_int;
+    public int bit_len;
+    public String desc;
+}
---
+++ src/net/middlemind/GenAsm/JsonObjIsRegisters.java
@@ -0,0 +1,13 @@
+package net.middlemind.GenAsm;
+
+import java.util.List;
+
+/**
+ *
+ * @author Victor G. Brusca, Middlemind Games 08/02/2021 12:58 PM EST
+ */
+public class JsonObjIsRegisters {
+    public String obj_name;
+    public String set_name;
+    public List<JsonObjIsRegister> is_registers;
+}
