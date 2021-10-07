dbd6ca71b4962989c8ce320158e72a8a456abab4
Victor G. Brusca <brusca@middlemind.net>
2021-10-07 12:30:14 -0400

    Updated project with jUnit lib adjustment and placeholder classes for enticipated unit tests.

---
+++ test/net/middlemind/GenAsm/Tests/Thumb/TestThumbPreProcessor.java
@@ -0,0 +1,40 @@
+package net.middlemind.GenAsm.Tests.Thumb;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Victor G. Brusca, Middlemind Games 10-07-2021 12:25 PM EST
+ */
+public class TestThumbPreProcessor {
+    
+    public TestThumbPreProcessor() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
