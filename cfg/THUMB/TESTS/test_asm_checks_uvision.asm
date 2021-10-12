169bf8e0ccf347ec96d18e6659b08eedbbe40756
Victor G. Brusca <brusca@middlemind.net>
2021-10-12 13:43:50 -0400

    Updated project with the latest changes to support verifyng assembly output.

---
+++ cfg/THUMB/TESTS/test_asm_checks_uvision.asm
@@ -0,0 +1,9 @@
+AREA ToThumb, CODE, READONLY  ; Name this block of code
+     ENTRY                         ; Mark first instruction to execute
+     ARM                           ; Subsequent instructions are ARM 
+start
+     ADR     r0, into_thumb + 1    ; Processor starts in ARM state 
+     BX      r0                    ; Inline switch to Thumb state
+     THUMB                         ; Subsequent instructions are Thumb
+into_thumb
+     MOVS    r0, #10 
