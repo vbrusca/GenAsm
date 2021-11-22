b94f60f2fef77346e2587d052256313990e4ec24
vbrusca <brusca@middlemind.net>
2021-11-22 11:47:12 -0500

    Added support for test program M
    Updated project with test program M support. This program is used to let the user provide their own name for the first program run in the text.

---
+++ cfg/THUMB/OUTPUT/TEST_C_OnesComp/run.bat
@@ -0,0 +1,13 @@
+@echo off
+@echo %1
+if [%~1]==[-vb] (goto startVB) else (goto startMGBA)
+
+:startVB
+N:\Emu\VisualBoyAdvance\VisualBoyAdvance "C:\Users\variable\Documents\GitHub\GenAsm\cfg\THUMB\OUTPUT\TEST_C_OnesComp\output_assembly_listing_endian_lil.bin"
+goto end
+
+:startMGBA
+N:\Emu\mGBA\mGBA "C:\Users\variable\Documents\GitHub\GenAsm\cfg\THUMB\OUTPUT\TEST_C_OnesComp\output_assembly_listing_endian_lil.bin"
+goto end
+
+:end
--- cfg/THUMB/OUTPUT/TEST_L_HelloWorld/test.bat
+++
@@ -1,1 +0,0 @@
-C:\Users\variable\Documents\GitHub\GbaAssemblyChecks\chibi_akumas\ARMDevTools\Emu\mGBA\mGBA.exe output_assembly_listing_endian_lil.bin
