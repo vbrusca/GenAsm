248c0c821791a1a0a6a275444c8900614bac5d40
vbrusca <brusca@middlemind.net>
2021-11-25 10:12:58 -0500

    Added work on emulators and run files
    Updated project with the latest changes to support emulators and relative directory run files.

--- cfg/THUMB/OUTPUT/TEST_J_16BitSeriesAddOne/run.bat
+++ cfg/THUMB/OUTPUT/TEST_J_16BitSeriesAddOne/run.bat
@@ -3,11 +3,11 @@
 if [%~1]==[-vb] (goto startVB) else (goto startMGBA)
 
 :startVB
-N:\Emu\VisualBoyAdvance\VisualBoyAdvance ".\output_assembly_listing_endian_lil.bin"
+..\..\EMUS\VisualBoyAdvance\VisualBoyAdvance ".\output_assembly_listing_endian_lil.bin"
 goto end
 
 :startMGBA
-N:\Emu\mGBA\mGBA ".\output_assembly_listing_endian_lil.bin"
+..\..\EMUS\mGBA\mGBA ".\output_assembly_listing_endian_lil.bin"
 goto end
 
 :end
